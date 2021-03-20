3c6edb90d637527ebadeb87a28597f29fb93c80f
DaFonz <brandonjfontes@gmail.com>
2021-03-20 00:18:57 -0600

    mongoose is connected

--- server.js
+++ server.js
@@ -15,9 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected');
 });
 
 // routes
